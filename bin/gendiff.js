ac8d50ef6a5da2cf1eea97526ca78373e43e673f
sergei mishin <mishins.s@yandex.ru>
2022-05-03 18:19:34 +0300

    second step complit

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,13 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference')
   .version('0.0.1', '-V, --version', 'output the version number')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  
+  
   
 
-program.parse();
+program.parse(process.argv);
 	
